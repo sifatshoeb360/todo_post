02821eb3735095f79c95fcd8b2a00bc13a7aac4e
shoebsift360 <shoebsifat360@gmail.com>
2022-06-13 00:26:59 +0600

    first commit

---
+++ eve.js
@@ -0,0 +1,71 @@
+let nameArray=[];
+
+let texArray=[];
+
+function addName(name){
+    if(name){
+        nameArray.push(name);
+    }
+}
+
+function addText(msg){
+    if(msg){
+        texArray.push(msg);
+    }
+}
+
+
+
+
+
+
+document.getElementById("submit").addEventListener('click',() => {
+
+    let name=document.getElementById("name").value;
+
+    addName(name);
+
+    let msg=document.getElementById("msg").value;
+
+    addText(msg);
+
+   
+    document.getElementById("wraper").innerHTML="";
+    
+    for(let i=0;i<nameArray.length;i++){
+
+        let mainDiv=document.createElement("div");
+
+        mainDiv.className="col-md-4 col-sm-6 mb-4";
+
+        let div=document.createElement("div");
+
+        div.className="my-border shadow-lg p-3";
+
+        let h3=document.createElement("h3");
+
+        h3.className="text-success"
+
+        h3.innerText=nameArray[i];
+
+        div.appendChild(h3);
+
+        let p=document.createElement("p");
+
+        p.className="text-white fs-6"
+
+        p.innerText=texArray[i];
+
+        div.appendChild(p);
+
+        mainDiv.appendChild(div);
+
+        document.getElementById("wraper").appendChild(mainDiv);
+
+        
+    }
+    
+    
+    
+})
+
